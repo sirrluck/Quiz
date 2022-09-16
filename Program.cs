c12d2b7a69ca5f5af24ab8e3d76e4671655d6da1
sirrluck <siriluck.kaew@kmutt.ac.th>
2022-09-16 11:52:03 +0700

    Quiz

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿public class KFC {
+    public string capital;
+    public string branchName;
+    public string address;
+    
+    public KFC(string capital, string branchName, string address) {
+        this.capital = capital;
+        this.branchName = branchName;
+        this.address = address;
+    }
+    
+     public string PrintInformation() {
+        return "information is : " 
+        + this.capital + " " +
+         this.branchName + " " +
+         this.address + " " +
+    }
+}
+
+public class User {
+    public string name;
+    public string surname;
+    public string citizenID;
+    
+   public User(string name, string surname, string studentID, int score) {
+        this.name = name;
+        this.surname = surname;
+        this.citizenID = citizenID;
+   }
+    
+     public string PrintUserInformation() {
+        return "User information is : " 
+        + this.name + " " + this.surname + " " +
+         this.citizenID;
+    }
